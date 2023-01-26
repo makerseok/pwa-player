efbf7912db48299c6a844625e7bd2255f4d9644e
makerseok <maker7788@gmail.com>
2023-01-26 13:38:05 +0900

    start_url 절대경로로 변경

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 'site-static-115';
-const DYNAMIC_CACHE_NAME = 'site-dynamic-v106';
+const STATIC_CACHE_NAME = 'site-static-116';
+const DYNAMIC_CACHE_NAME = 'site-dynamic-v107';
 const VIDEO_CACHE_NAME = 'site-video-v4';
 const FONT_CACHE_NAME = 'site-font-v1';
 const APEX_CACHE_NAME = 'site-apex-v1';
