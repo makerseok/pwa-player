d92d8a72ca672eb7d2eafe54ba5b9578ffe8e9ff
makerseok <maker7788@gmail.com>
2023-01-26 18:45:53 +0900

    html lang 변경

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 'site-static-124';
-const DYNAMIC_CACHE_NAME = 'site-dynamic-v115';
+const STATIC_CACHE_NAME = 'site-static-125';
+const DYNAMIC_CACHE_NAME = 'site-dynamic-v116';
 const VIDEO_CACHE_NAME = 'site-video-v4';
 const FONT_CACHE_NAME = 'site-font-v1';
 const APEX_CACHE_NAME = 'site-apex-v1';
