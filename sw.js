6131d8424a9f1a1f745488187d28f6aebdd0b57b
makerseok <maker7788@gmail.com>
2023-01-26 15:50:51 +0900

    orientation 속성 변경

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 'site-static-119';
-const DYNAMIC_CACHE_NAME = 'site-dynamic-v110';
+const STATIC_CACHE_NAME = 'site-static-120';
+const DYNAMIC_CACHE_NAME = 'site-dynamic-v111';
 const VIDEO_CACHE_NAME = 'site-video-v4';
 const FONT_CACHE_NAME = 'site-font-v1';
 const APEX_CACHE_NAME = 'site-apex-v1';
